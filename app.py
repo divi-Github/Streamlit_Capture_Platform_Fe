8da71ee4cefc84d61541aab896834191880996f3
divi-Github <180757243+divi-Github@users.noreply.github.com>
2025-10-10 12:30:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -542,7 +542,7 @@ st.set_page_config(page_title="Capture Platform", layout="wide",
 
 # API base URL for FastAPI backend
 # API_BASE_URL = "https://tuboid-nonparochially-christian.ngrok-free.dev/data"  
-API_BASE_URL = "https://brunswick-pond-addresses-gpl.trycloudflare.com//data"  
+API_BASE_URL = "https://brunswick-pond-addresses-gpl.trycloudflare.com/data"  
 # API_BASE_URL = "http://localhost:8003/data"  
 MODEL3_API_BASE_URL = "https://unstandardisable-shantelle-gnawable.ngrok-free.dev" 
 # MODEL3_API_BASE_URL = "https://tuboid-nonparochially-christian.ngrok-free.dev" 
@@ -789,7 +789,7 @@ def process_files_tab():
         endpoint = f"/{api_choice[0]}"
         # Use different base URL for Model-3 (when api_choice[0] == "upload")
         # base_url = MODEL3_API_BASE_URL if api_choice[0] == "upload" else "http://localhost:8000"
-        base_url = MODEL3_API_BASE_URL if api_choice[0] == "upload" else "https://brunswick-pond-addresses-gpl.trycloudflare.com/"
+        base_url = MODEL3_API_BASE_URL if api_choice[0] == "upload" else "https://brunswick-pond-addresses-gpl.trycloudflare.com"
         # base_url = MODEL3_API_BASE_URL if api_choice[0] == "upload" else "https://tuboid-nonparochially-christian.ngrok-free.dev"
         # base_url = MODEL3_API_BASE_URL if api_choice[0] == "upload" else "https://tuboid-nonparochially-christian.ngrok-free.dev"
         url = base_url + endpoint
